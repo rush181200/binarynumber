87eb837fff0827924c2d943e8bee873a81dce236
rush181200 <rushabhm75@gmail.com>
2020-08-14 23:37:27 +0530

    done

--- lib/pages/showspage.dart
+++ lib/pages/showspage.dart
@@ -25,198 +25,214 @@ class _ShowsPageState extends State<ShowsPage> {
 
     return Container(
       margin: EdgeInsets.all(10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            child: Text(
-              'NEW',
-              style:
-                  TextStyle(color: Theme.of(context).accentColor, fontSize: 30),
-            ),
-          ),
-          Container(
-            height: height * 0.5,
-            width: width,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: mp.itemCount,
-                itemBuilder: (context, i) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (ctx) => ShowDetails(
-                                imdbid: mp.searchlist[i].imdbid,
-                              )));
-                    },
-                    child: Container(
-                        margin: EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Container(
-                              height: height * 0.39,
-                              width: width * 0.4,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Theme.of(context).disabledColor,
-                                image: DecorationImage(
-                                    image:
-                                        NetworkImage(mp.searchlist[i].poster),
-                                    fit: BoxFit.fill),
-                              ),
-                            ),
-                            Transform.translate(
-                              offset: Offset(0, -35),
-                              child: Container(
-                                  child: Container(
-                                height: height * 0.08,
-                                width: width * 0.2,
-                                decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Center(
-                                  child: Text(
-                                    'New',
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Theme.of(context).accentColor),
+      child: mp == null
+          ? Container(
+              margin: EdgeInsets.only(
+                  top: height * 0.5,
+                  bottom: height * 0.5,
+                  left: width * 0.3,
+                  right: width * 0.3),
+              height: height * 0.05,
+              width: width * 0.05,
+              child: CircularProgressIndicator())
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Text(
+                    'NEW',
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor, fontSize: 30),
+                  ),
+                ),
+                Container(
+                  height: height * 0.5,
+                  width: width,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: mp.itemCount,
+                      itemBuilder: (context, i) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (ctx) => ShowDetails(
+                                      imdbid: mp.searchlist[i].imdbid,
+                                    )));
+                          },
+                          child: Container(
+                              margin: EdgeInsets.all(10),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: height * 0.39,
+                                    width: width * 0.4,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Theme.of(context).disabledColor,
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                              mp.searchlist[i].poster),
+                                          fit: BoxFit.fill),
+                                    ),
                                   ),
-                                ),
+                                  Transform.translate(
+                                    offset: Offset(0, -35),
+                                    child: Container(
+                                        child: Container(
+                                      height: height * 0.08,
+                                      width: width * 0.2,
+                                      decoration: BoxDecoration(
+                                          color: Colors.green,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Center(
+                                        child: Text(
+                                          'New',
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Theme.of(context)
+                                                  .accentColor),
+                                        ),
+                                      ),
+                                    )),
+                                  )
+                                ],
                               )),
-                            )
-                          ],
-                        )),
-                  );
-                }),
-          ),
-          //---------------------------------------------------------------------
-          Container(
-            child: Text(
-              'POPULAR',
-              style:
-                  TextStyle(color: Theme.of(context).accentColor, fontSize: 30),
-            ),
-          ),
-          Container(
-            height: height * 0.5,
-            width: width,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: mp.searchlist.length,
-                itemBuilder: (context, i) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (ctx) => ShowDetails(
-                                imdbid: mp.searchlist[i].imdbid,
-                              )));
-                    },
-                    child: Container(
-                        margin: EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Container(
-                              height: height * 0.39,
-                              width: width * 0.4,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Theme.of(context).disabledColor,
-                                  image: DecorationImage(
-                                      image:
-                                          NetworkImage(mp.searchlist[i].poster),
-                                      fit: BoxFit.fill)),
-                            ),
-                            Transform.translate(
-                              offset: Offset(0, -35),
-                              child: Container(
-                                  child: Container(
-                                height: height * 0.08,
-                                width: width * 0.2,
-                                decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Center(
-                                  child: Text(
-                                    'popular',
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Theme.of(context).accentColor),
+                        );
+                      }),
+                ),
+                //---------------------------------------------------------------------
+                Container(
+                  child: Text(
+                    'POPULAR',
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor, fontSize: 30),
+                  ),
+                ),
+                Container(
+                  height: height * 0.5,
+                  width: width,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: mp.searchlist.length,
+                      itemBuilder: (context, i) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (ctx) => ShowDetails(
+                                      imdbid: mp.searchlist[i].imdbid,
+                                    )));
+                          },
+                          child: Container(
+                              margin: EdgeInsets.all(10),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: height * 0.39,
+                                    width: width * 0.4,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Theme.of(context).disabledColor,
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                mp.searchlist[i].poster),
+                                            fit: BoxFit.fill)),
                                   ),
-                                ),
+                                  Transform.translate(
+                                    offset: Offset(0, -35),
+                                    child: Container(
+                                        child: Container(
+                                      height: height * 0.08,
+                                      width: width * 0.2,
+                                      decoration: BoxDecoration(
+                                          color: Colors.green,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Center(
+                                        child: Text(
+                                          'popular',
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Theme.of(context)
+                                                  .accentColor),
+                                        ),
+                                      ),
+                                    )),
+                                  )
+                                ],
                               )),
-                            )
-                          ],
-                        )),
-                  );
-                }),
-          ),
-          //--------------------------------------------------------------
-          Container(
-            child: Text(
-              'TRENDING LIST',
-              style:
-                  TextStyle(color: Theme.of(context).accentColor, fontSize: 30),
-            ),
-          ),
-          Container(
-            height: height * 0.5,
-            width: width,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: mp.searchlist.length,
-                itemBuilder: (context, i) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (ctx) => ShowDetails(
-                                imdbid: mp.searchlist[i].imdbid,
-                              )));
-                    },
-                    child: Container(
-                        margin: EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Container(
-                              height: height * 0.39,
-                              width: width * 0.4,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Theme.of(context).disabledColor,
-                                  image: DecorationImage(
-                                      image:
-                                          NetworkImage(mp.searchlist[i].poster),
-                                      fit: BoxFit.fill)),
-                            ),
-                            Transform.translate(
-                              offset: Offset(0, -35),
-                              child: Container(
-                                  child: Container(
-                                height: height * 0.08,
-                                width: width * 0.2,
-                                decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Center(
-                                  child: Text(
-                                    'TOP 5',
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Theme.of(context).accentColor),
+                        );
+                      }),
+                ),
+                //--------------------------------------------------------------
+                Container(
+                  child: Text(
+                    'TRENDING LIST',
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor, fontSize: 30),
+                  ),
+                ),
+                Container(
+                  height: height * 0.5,
+                  width: width,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: mp.searchlist.length,
+                      itemBuilder: (context, i) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (ctx) => ShowDetails(
+                                      imdbid: mp.searchlist[i].imdbid,
+                                    )));
+                          },
+                          child: Container(
+                              margin: EdgeInsets.all(10),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: height * 0.39,
+                                    width: width * 0.4,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Theme.of(context).disabledColor,
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                mp.searchlist[i].poster),
+                                            fit: BoxFit.fill)),
                                   ),
-                                ),
+                                  Transform.translate(
+                                    offset: Offset(0, -35),
+                                    child: Container(
+                                        child: Container(
+                                      height: height * 0.08,
+                                      width: width * 0.2,
+                                      decoration: BoxDecoration(
+                                          color: Colors.green,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Center(
+                                        child: Text(
+                                          'TOP 5',
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Theme.of(context)
+                                                  .accentColor),
+                                        ),
+                                      ),
+                                    )),
+                                  )
+                                ],
                               )),
-                            )
-                          ],
-                        )),
-                  );
-                }),
-          ),
-        ],
-      ),
+                        );
+                      }),
+                ),
+              ],
+            ),
     );
   }
 }
